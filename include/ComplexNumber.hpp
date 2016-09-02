a857566911607989d774defbe57fb08471877bac
Konstantinos Kazatzis <K.Kazatzis@Innora.local>
2016-09-02 16:34:19 +0300

    added lines in another file

--- include/ComplexNumber.hpp
+++ include/ComplexNumber.hpp
@@ -6,53 +6,67 @@ namespace ComplexNumber{
 		private:
 			double real;
 			double imaginary;
-		public:
 
+		public:
 			complex_number(){
 				real=0.0;
 				imaginary=0.0;
 			};
+
 			complex_number(const double new_real, const double new_imaginary){
 				real = new_real;
 				imaginary = new_imaginary;
 			}
+
 			double getRealPart(){
 				return real;
 			}
+
 			double getImaginaryPart(){
 				return imaginary;
 			}
+
 			complex_number getConjugate(){
 				return {real, -1 * imaginary};
 			}
+
 			double getAbsoluteValue(){
 				return sqrt(real*real + imaginary*imaginary);
 			}
+
 			double getSquareOfAbsoluteValue(){
 				return real*real + imaginary*imaginary;
 			}
+
 			double getPhase(){
 				return atan2(imaginary,real);
 			}
+
 			complex_number operator*(complex_number cm1){
 				return {real*cm1.real - imaginary*cm1.imaginary,real*cm1.imaginary + imaginary*cm1.real};
 			}
+
 			complex_number operator/(complex_number cm1){
 				return {(real*cm1.real+imaginary*cm1.imaginary)/(cm1.real*cm1.real + cm1.imaginary*cm1.imaginary),(imaginary*cm1.real-real*cm1.imaginary)/(cm1.real*cm1.real + cm1.imaginary*cm1.imaginary)};
 			}
+
 			complex_number operator+(complex_number cm1){
 				return {real + cm1.real,imaginary + cm1.imaginary};
 			}
+
 			complex_number operator-(complex_number cm1){
 				return {real - cm1.real,imaginary - cm1.imaginary};
 			}
+
 			bool operator==(complex_number cm1){
 				return(real==cm1.real && imaginary==cm1.imaginary);
 			}
+
 			void operator=(const complex_number &cm1){
 				real = cm1.real;
 				imaginary = cm1.imaginary;
 			}
+
 			friend std::ostream& operator<<(std::ostream& stream, const complex_number &cm1){
 				if (cm1.real!=0) std::cout << cm1.real ;
 				if (cm1.imaginary < 0) std::cout << cm1.imaginary;
@@ -61,5 +75,6 @@ namespace ComplexNumber{
 				std::cout <<"i";
 				return stream;
 			}
+
 	};
 }
